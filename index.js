97f88fed2b19209786a71291a182621522567545
FiguringToCode <marineryash2000@gmail.com>
2025-09-04 11:24:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ app.get('/homeAppliances', async (req, res) => {
 
 
 
-const PORT=process.env.PORT
+const PORT=process.env.MONGODB
 app.listen(PORT, () => {
     console.log("Server connected to port", PORT)
 })
